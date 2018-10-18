00d0ae60ab7b4192f1153f04e77a9820bc285a1e
Tomas Hrcka <thrcka@redhat.com>
2018-10-18 23:00:17 +0200

    update endpoint

--- src/app.py
+++ src/app.py
@@ -1,23 +1,25 @@
-from flask import Flask
-import smbus2
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import bme280 as thermometer
+import smbus2
+from flask import Flask, jsonify
 
 port = 0
 address = 0x77
 bus = smbus2.SMBus(port)
-
-calibration_params = thermometer.load_calibration_params(bus, address)
-
-# the sample method will take a single reading and return a
-# compensated_reading object
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    calibration_params = thermometer.load_calibration_params(bus, address)
     data = thermometer.sample(bus, address, calibration_params)
-    return data
+    return jsonify({'time': data.timestamp,
+                    'humidity': data.humidity,
+                    'temperature': data.temperature,
+                    'pressure': data.pressure})
 
 
-app.run()
+app.run(host= '192.168.88.122', port=9000, debug=False)
